f372dda029f597f9f548fc7dd7c1456b93969074
parameshwar <parameshwar273@gmail.com>
2023-01-14 05:25:09 -0500

    Added comments for mayakam rule 1

--- Mayakam_rule1.py
+++ Mayakam_rule1.py
@@ -1,5 +1,8 @@
 import tamil
 
+# 1) ட் ற் ல் ள் + க ச ப
+# ஒரு சொல்லில் ட், ற், ல், ள் ஆகிய எழுத்துக்களுக்குப் பின்பு ஆகிய க, ச, ப ஆகிய எழுத்துக்கள் மயங்கி வரும். எ-டு. கேட்க, கற்க, செல்க, கொள்க.
+
 word=input("Enter the word to check: ")
 letters=tamil.utf8.get_letters(word)
 if "ட்" in letters and letters.index("ட்")!=len(letters)-1:
