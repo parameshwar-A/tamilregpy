e4a21ffcd99ca5baf60d6f350a976276160dd42f
parameshwar <parameshwar273@gmail.com>
2023-01-18 09:03:46 -0500

    Fixed the root word logic I told

--- Mayakam_rule1.py
+++ Mayakam_rule1.py
@@ -6,28 +6,48 @@ import tamil
 word=input("Enter the word to check: ")
 letters=tamil.utf8.get_letters(word)
 if "ட்" in letters and letters.index("ட்")!=len(letters)-1:
-	ind=letters.index("ட்")
-	if letters[ind+1]=="க" or  letters[ind+1]=="ச" or letters[ind+1]=="ப":
-		print(True)
-	else:
-		print(False)
+    ind=letters.index("ட்")
+    root_words=tamil.utf8.splitMeiUyir(letters[ind+1])
+    if type(root_words)==tuple:
+        root_last=root_words[0]
+    else:
+        root_last=root_words
+    if root_last=="க்" or  root_last=="ச்" or root_last=="ப்":
+        print(True)
+    else:
+        print(False)
 elif "ற்" in letters and letters.index("ற்")!=len(letters)-1:
-	ind=letters.index("ற்")
-	if letters[ind+1]=="க" or  letters[ind+1]=="ச" or letters[ind+1]=="ப":
-		print(True)
-	else:
-		print(False)
+    ind=letters.index("ற்")
+    root_words=tamil.utf8.splitMeiUyir(letters[ind+1])
+    if type(root_words)==tuple:
+        root_last=root_words[0]
+    else:
+        root_last=root_words
+    if root_last=="க்" or  root_last=="ச்" or root_last=="ப்":
+        print(True)
+    else:
+        print(False)
 elif "ல்" in letters and letters.index("ல்")!=len(letters)-1:
-	ind=letters.index("ல்")
-	if letters[ind+1]=="க" or  letters[ind+1]=="ச" or letters[ind+1]=="ப":
-		print(True)
-	else:
-		print(False)
+    ind=letters.index("ல்")
+    root_words=tamil.utf8.splitMeiUyir(letters[ind+1])
+    if type(root_words)==tuple:
+        root_last=root_words[0]
+    else:
+        root_last=root_words
+    if root_last=="க்" or  root_last=="ச்" or root_last=="ப்":
+        print(True)
+    else:
+        print(False)
 elif "ள்" in letters and letters.index("ள்")!=len(letters)-1:
-	ind=letters.index("ள்")
-	if letters[ind+1]=="க" or  letters[ind+1]=="ச" or letters[ind+1]=="ப":
-		print(True)
-	else:
-		print(False)
+    ind=letters.index("ள்")
+    root_words=tamil.utf8.splitMeiUyir(letters[ind+1])
+    if type(root_words)==tuple:
+        root_last=root_words[0]
+    else:
+        root_last=root_words
+    if root_last=="க்" or  root_last=="ச்" or root_last=="ப்":
+        print(True)
+    else:
+        print(False)
 else:
-	print(False)
+    print(False)
